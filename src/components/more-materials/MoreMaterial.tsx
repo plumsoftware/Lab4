c0ee203088c17de5997c927ade5efae69e8658ea
Slava <plumsoftwareofficial@gmail.com>
2023-12-23 23:22:27 +0600

    [+] simple code changes

--- src/components/more-materials/MoreMaterial.tsx
+++ src/components/more-materials/MoreMaterial.tsx
@@ -1,5 +1,6 @@
 import classes from './MoreMaterial.module.css' 
 import React from 'react' 
+import Divider from '../Divider';
  
 interface PromoItem {
     title: string;
@@ -13,17 +14,22 @@ interface MoreProps {
 
 const More: React.FC<MoreProps> = ( {arr} ) => { 
     return ( 
-         <div className={classes.row}>
+        
+        <div className={classes.column}>
+            <h1 className={classes.title_text}>Check out our latest article</h1>
+            <Divider/>
 
-            {arr.map((item, index) => (
-                    <div key={index} className={classes.cardMaterial}>
-                        <img className={classes.promo} src={arr[index].img} />
-                        <h1 className={classes.h1}>{arr[index].title}</h1>
-                        <h2 className={classes.h2}>{arr[index].subtitle}</h2>
-                        <button className={classes.moreButton}>Read more ➜</button>
-                    </div>
-                ))}
-         </div>
+            <div className={classes.row}>
+                {arr.map((item, index) => (
+                        <div key={index} className={classes.cardMaterial}>
+                            <img className={classes.promo} src={arr[index].img} />
+                            <h1 className={classes.h1}>{arr[index].title}</h1>
+                            <h2 className={classes.h2}>{arr[index].subtitle}</h2>
+                            <button className={classes.moreButton}>Read more ➜</button>
+                        </div>
+                    ))}
+            </div>
+        </div>
     ); 
 }; 
  
